ecd8be53e59053c29b5460cc0322811425d4bbaa
Profpatsch <mail@profpatsch.de>
2017-05-18 06:25:33 +0200

    generate nix file with shortened package functions
    We now have a pretty solid output that is 2/3 of the original `yarn.lock` for
    our example.
    Next step is to actually use the multi-keyed map. Hm, maybe that might even
    bring the line-count *up* again, but should reduce file size (because package
    definitions and thus hash sums will be shared.

--- src/Distribution/Nixpkgs/Nodejs/FromYarnLock.hs
+++ src/Distribution/Nixpkgs/Nodejs/FromYarnLock.hs
@@ -5,20 +5,27 @@ module Distribution.Nixpkgs.Nodejs.FromYarnLock
 ) where
 
 import Protolude
+import Control.Monad.Writer.Lazy (Writer, tell, runWriter)
 import Data.Fix (Fix(Fix))
+import qualified Data.Text as T
+import qualified Data.Map as M
 import Nix.Expr
 import Nix.Pretty (prettyNix)
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 import Text.Regex.TDFA.Text ()
 import Text.Regex.TDFA ((=~))
 
+import qualified Data.MultiKeyedMap as MKM
 import Yarn.Lock (PackageKey(..), Package(..), Lockfile, RemoteFile(..))
 import qualified Yarn.Lock as YL
 
 
 -- | Pretty print the nixpkgs version of @yarn.lock@ to stdout.
 toStdout  :: Lockfile -> IO ()
-toStdout = PP.putDoc . prettyNix . mkPackageSet
+toStdout lf = do
+  let (res, warnings) = runWriter $ mkPackageSet lf
+
+  PP.putDoc . prettyNix $ res
 
 -- CONVENTION: *Sym is a nix symbol
 -- We keep them Text, because that’s what they are in hnix
@@ -54,6 +61,8 @@ data RegVal = V AntiQ
 data Registry = Registry
   { registrySym :: Text
   -- ^ registry symbol name
+  , registryShortSym :: ShortSym
+  -- ^ shortened symbol name
   , registryPrefix :: Text
   -- ^ prefix string that selects to this prefix
   , buildRegistry :: AntiQ -> AntiQ -> [RegVal]
@@ -64,15 +73,18 @@ data Registry = Registry
 newtype ShortSym = ShortSym { unShortSym :: Text } deriving (IsString)
 
 yarnRegistry :: Registry
-yarnRegistry = Registry "yarnpkg" p
+yarnRegistry = Registry "yarnpkg" "y" p
                 $ \n v -> [T p, V n, T "/-/", V n, T "-", V v, T ".tgz"]
   where p = "https://registry.yarnpkg.com/"
 
 -- | all Registries with their shortened names
-allRegistryTuples :: [(Registry, ShortSym)]
-allRegistryTuples = map (fmap ShortSym)
-  [ (yarnRegistry, "y")]
--- TODO: include ShortSym in the actual registry?
+allRegistries :: [Registry]
+allRegistries = [yarnRegistry]
+
+recognizeRegistry :: Text -> Maybe Registry
+recognizeRegistry t = listToMaybe
+  $ filter (\Registry{..} -> registryPrefix `T.isPrefixOf` t)
+  $ allRegistries
 
 prefixesSym :: Text
 prefixesSym = "prefixes"
@@ -83,23 +95,28 @@ data ShortenedBinding = ShortenedBinding
   , bindingShortSym :: ShortSym
   , binding :: NExpr }
 
+-- | helper to get the short sym out as 'NExpr'
+shortSym :: ShortenedBinding -> NExpr
+shortSym = mkSym . unShortSym . bindingShortSym
+
 pkgBuildFn :: ShortenedBinding
 pkgBuildFn = ShortenedBinding "shortBuildPkg" "b" bnd
   where
-    bnd = multiParam ["name", "version", "prfx", "sha1", "deps"] $ mkNonRecSet
-           [ inherit [StaticKey "name", StaticKey "version"]
-           , "src" $= (mkSym fetchurlSym @@ mkNonRecSet
-             [ "url" $= ("prfx" @@ "name" @@ "version")
-             , inherit [StaticKey "sha1"] ])
-           , "nodeBuildInputs" $= "deps"
-           ]
+    bnd = multiParam ["name", "version", "prfx", "sha1", "deps"]
+            $ mkSym buildNodePackageSym @@ mkNonRecSet
+              [ inherit [StaticKey "name", StaticKey "version"]
+              , "src" $= (mkSym fetchurlSym @@ mkNonRecSet
+                [ "url" $= ("prfx" @@ "name" @@ "version")
+                , inherit [StaticKey "sha1"] ])
+              , "nodeBuildInputs" $= "deps"
+              ]
 
-allShortenedBindings :: [ShortenedBinding]
+allShortenedBindings  :: [ShortenedBinding]
 allShortenedBindings = [pkgBuildFn]
 
-registryShortenedBinding :: (Registry, ShortSym) -> ShortenedBinding
-registryShortenedBinding (Registry{registrySym, buildRegistry}, s) =
-  ShortenedBinding registrySym s $ multiParam ["n", "v"]
+registryShortenedBinding :: Registry -> ShortenedBinding
+registryShortenedBinding Registry{registrySym, registryShortSym, buildRegistry} =
+  ShortenedBinding registrySym registryShortSym $ multiParam ["n", "v"]
     $ regvToNExpr $ buildRegistry (Antiquoted "n") (Antiquoted "v")
   where
     regvToNExpr regvals = Fix . NStr . DoubleQuoted
@@ -122,11 +139,11 @@ staticBindings = longDefinitions ++ shortenedDefinitions
     longDefinitions = prefixAttrset : mkSymBnds allShortenedBindings
     -- the definition attrset of prefixes
     prefixAttrset = prefixesSym $= mkNonRecSet
-      (mkSymBnds $ map registryShortenedBinding allRegistryTuples)
+      (mkSymBnds $ map registryShortenedBinding allRegistries)
     -- list of shortened definitions
     shortenedDefinitions = mkShortSymBnds $
       (map (ln Nothing)             allShortenedBindings) ++
-      (map (ln (Just prefixesSym) . registryShortenedBinding) allRegistryTuples)
+      (map (ln (Just prefixesSym) . registryShortenedBinding) allRegistries)
     -- link to the full definition that has maybe a prefix
     ln :: Maybe Text -> ShortenedBinding -> ShortenedBinding
     ln expr sb@ShortenedBinding{bindingSym} =
@@ -135,24 +152,37 @@ staticBindings = longDefinitions ++ shortenedDefinitions
           Nothing  -> replRHS $ mkSym bindingSym
           Just prf -> replRHS $ mkSym prf !!. bindingSym
 
+-- TODO: improve
+data Warning = Warn Text
 
 -- | Convert a @yarn.lock@ to a nix expression.
-mkPackageSet :: Lockfile -> NExpr
-mkPackageSet (YL.decycle -> lf) =
-  simpleParamSet [buildNodePackageSym, fixSym, fetchurlSym] ==>
+mkPackageSet :: Lockfile -> Writer [Warning] NExpr
+mkPackageSet (YL.decycle -> lf) = do
+  pkgMap <- sequence $ M.mapWithKey recognizeOrWarn
+    -- TODO: use true PackageKey mappings, not the flat version
+           $ MKM.flattenKeys lf
+
+  pure $ simpleParamSet [buildNodePackageSym, fixSym, fetchurlSym] ==>
     -- enable self-referencing of packages
     -- with string names with a shallow fix
     -- see note FIX
-    mkLets staticBindings  -- ++ ["pkgs" (Param selfPkgSym  ==> pkgSet)]
+    mkLets (staticBindings ++ ["pkgs" $= (Param selfPkgSym  ==> pkgSet pkgMap)])
       (mkSym fixSym @@ "pkgs")
   where
-    -- | set of all packages
-    -- TODO: use true PackageKey mappings, not the flat version
-    -- pkgSet = mkNonRecSet (M.elems $ M.mapWithKey binding $ MKM.flattenKeys lf)
-    -- -- | the binding of a single package
-    -- binding :: PackageKey -> Package -> Binding NExpr
-    -- binding pkgKey pkg = packageKeyToIdentifier pkgKey
-    --                      $$= mkPackage (name pkgKey) pkg
+    -- | set of all package
+    recognizeOrWarn :: PackageKey -> Package -> Writer [Warning] NExpr
+    recognizeOrWarn key pkg = case recognizeRegistry regStr of
+         Nothing -> tell [tmpWarn] >> pure (mk $mkStr regStr)
+         Just reg -> pure $ mk (shortSym $ registryShortenedBinding reg)
+      where
+        mk x = mkPackage (name key) x pkg
+        regStr = url $ resolved pkg
+        tmpWarn :: Warning
+        tmpWarn = Warn $ "Package `" <> packageKeyToIdentifier key
+               <> "` contains unknown registry, " <> url (resolved pkg)
+    pkgSet :: Map PackageKey NExpr -> NExpr
+    pkgSet = mkNonRecSet . M.elems . M.mapWithKey
+               (\key val -> packageKeyToIdentifier key $$= val)
 
 {- NOTE fix
 If attributes in a rec set have string names
@@ -173,21 +203,14 @@ in fix attrs
 -}
 
 -- | A single package expression.
-mkPackage :: Text -> Package -> NExpr
-mkPackage name' pkg = mkSym buildNodePackageSym @@ mkNonRecSet
-  [ "name"    $= mkStr name'
-  , "version" $= mkStr (version pkg)
-  , "src"     $= fetchSrc (resolved pkg)
-  -- TODO: How to handle optional dependencies?
-  , "nodeBuildInputs"
-      $= mkList (((mkSym selfPkgSym) !!.) . packageKeyToIdentifier <$>
-        (dependencies pkg )) -- <> optionalDependencies pkg))
-  ]
+mkPackage :: Text -> NExpr -> Package -> NExpr
+mkPackage name' registry (Package ver remote deps optdeps) = (shortSym pkgBuildFn)
+  @@ mkStr name' @@ mkStr ver @@ registry
+  @@ mkStr (sha1sum remote) @@ depList
   where
-    fetchSrc :: RemoteFile -> NExpr
-    fetchSrc rf = mkSym fetchurlSym @@ mkNonRecSet
-                   [ "url"  $= mkStr (url rf)
-                   , "sha1" $= mkStr (sha1sum rf) ]
+    -- TODO: How to handle optional dependencies?
+    depList = mkList (((mkSym selfPkgSym) !!.)
+                      . packageKeyToIdentifier <$> (deps <> optdeps))
 
 -- | Representation of a PackageKey as nix attribute name.
 packageKeyToIdentifier :: PackageKey -> Text
