bf195e517fe5d1546788586a726990f9ef5ea32e
Profpatsch <mail@profpatsch.de>
2017-05-21 23:06:46 +0200

    shell.nix

---
+++ shell.nix
@@ -0,0 +1,28 @@
+((import <nixpkgs> {}).haskellPackages.override {
+  overrides = self: super: {
+        my-pkg = let
+          buildDepends = with self; [
+            protolude
+            megaparsec
+            tasty-th
+            HUnit
+            tasty-hunit
+            ansi-wl-pprint
+          ];
+          in super.mkDerivation {
+            pname = "pkg-env";
+            src = "/dev/null";
+            version = "none";
+            license = "none";
+            inherit buildDepends;
+            buildTools = with self; [
+              ghcid
+              cabal-install
+              hpack
+              (hoogleLocal {
+                packages = buildDepends;
+              })
+            ];
+          };
+      };
+}).my-pkg.env
