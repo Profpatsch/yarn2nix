efee0e672db1a3519977a52a63f0df35c7eb682f
Profpatsch <mail@profpatsch.de>
2017-05-22 00:01:17 +0200

    shell.nix

--- shell.nix
+++ shell.nix
@@ -1,1 +1,41 @@
-(import ./pkg.nix).env
+with import <nixpkgs> {};
+(haskellPackages.override {
+  overrides = self: super:
+    let yarn-lock = super.mkDerivation {
+          pname = "yarn-lock";
+          version = "0.1.0";
+          src = fetchFromGitHub {
+            owner = "Profpatsch";
+            repo = "yarn-lock";
+            rev = "0.2.0";
+            sha256 = "0cf1g2z6prkw6rs7a6kg685l9v9qxfcfd56wnff04hrrdb6rxdgf";
+          };
+          license = lib.licenses.mit;
+          buildDepends = with self; [ megaparsec protolude regex-tdfa regex-tdfa-text ansi-wl-pprint tasty-hunit tasty-th ];
+          buildTools = [ self.hpack ];
+          preConfigure = ''hpack'';
+        };
+     in {
+        my-pkg = let
+          buildDepends = with self; [
+            protolude
+            yarn-lock
+            hnix
+            hpack
+          ];
+          in super.mkDerivation {
+            pname = "pkg-env";
+            src = "/dev/null";
+            version = "none";
+            license = "none";
+            inherit buildDepends;
+            buildTools = with self; [
+              ghcid
+              cabal-install
+              (hoogleLocal {
+                packages = buildDepends;
+              })
+            ];
+          };
+      };
+}).my-pkg.env
