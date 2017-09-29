92ef60a06ddfd2803c545c9f21920d74dcacc8cd
Profpatsch <mail@profpatsch.de>
2017-09-29 03:51:24 +0200

    pooled, parallel git prefetch with status report channel

--- src/Distribution/Nixpkgs/Nodejs/FromYarnLock.hs
+++ src/Distribution/Nixpkgs/Nodejs/FromYarnLock.hs
@@ -3,7 +3,7 @@ module Distribution.Nixpkgs.Nodejs.FromYarnLock
 (
 -- ( toStdout
 -- , mkPackageSet
-resolveLockfile
+resolveLockfileStatus
 ) where
 
 import Protolude
@@ -11,6 +11,7 @@ import Control.Monad.Writer.Lazy (Writer, tell, runWriter)
 import qualified Control.Monad.Trans.Either as E
 import Data.Foldable (foldr1)
 import Data.Fix (Fix(Fix))
+import qualified Data.List.NonEmpty as NE
 import qualified Data.Text as T
 import qualified Data.Map as M
 import qualified Data.MultiKeyedMap as MKM
@@ -23,11 +24,15 @@ import Nix.Expr.Additions
 import Nix.Pretty (prettyNix)
 import qualified Text.PrettyPrint.ANSI.Leijen as PP
 import qualified Control.Concurrent.Async.Pool as Async
+import qualified Control.Monad.STM as STM
 
 import qualified Yarn.Lock as YL
 import qualified Yarn.Lock.Types as YLT
 import qualified Yarn.Lock.Helpers as YLH
 import Distribution.Nixpkgs.Nodejs.Utils
+
+maxFetchers :: Int
+maxFetchers = 5
 
 -- | A thing whose hash is already known (“resolved”).
 --
@@ -41,29 +46,33 @@ data Resolved a = Resolved
 type ResolvedLockfile = MKM.MKMap YLT.PackageKey (Resolved YLT.Package)
 
 -- | Resolve all packages by downloading their sources if necessary.
-resolveLockfile :: YLT.Lockfile -> IO ResolvedLockfile
-resolveLockfile = traverse (\(YLT.Package{..}) ->
-    resolve remote >>= \case
-      (Left err) -> panic err
-      (Right resRemote) -> do
-        case resRemote of
-          (Resolved _ r@YLT.GitRemote{..}) -> print r
-          _ -> pass
-        pure $ fmap (\remote -> YLT.Package{..}) resRemote)
+resolveLockfileStatus :: (Chan YLT.Remote) -> YLT.Lockfile
+                      -> IO (Either (NE.NonEmpty Text) ResolvedLockfile)
+resolveLockfileStatus msgChan lf = Async.withTaskGroup maxFetchers $ \taskGroup -> do
+  job <- STM.atomically $ Async.mapReduce taskGroup
+           $ fmap (\(ks, pkg) -> (:[]) <$> (E.runEitherT $ do
+                        liftIO $ writeChan msgChan (YLT.remote pkg)
+                        res <- resolve pkg
+                        pure (ks, res)))
+               $ MKM.toList lf
+  resolved <- Async.wait job
+  case partitionEithers resolved of
+    (x:xs, _ ) -> pure $ Left $ x NE.:| xs
+    (_   , ys) -> pure $ Right $ MKM.fromList YLT.lockfileIkProxy ys
+
   where
-    resolve :: YLT.Remote -> IO (Either Text (Resolved YLT.Remote))
-    resolve = E.runEitherT . \case
-      (resolved@YLT.FileRemote{..}) ->
-        pure $ Resolved{ sha1sum = fileSha1, .. }
-      (resolved@YLT.GitRemote{..}) -> do
-        sha1sum <- fetchFromGit gitRepoUrl gitRev
-        pure $ Resolved{..}
+    resolve :: YLT.Package -> E.EitherT Text IO (Resolved YLT.Package)
+    resolve pkg = case YLT.remote pkg of
+      YLT.FileRemote{..} -> pure $ r fileSha1
+      YLT.GitRemote{..}  -> r <$> fetchFromGit gitRepoUrl gitRev
+      where
+        r sha = Resolved { sha1sum = sha, resolved = pkg }
 
     fetchFromGit :: Text -> Text -> E.EitherT Text IO Text
     fetchFromGit repo rev = do
-      out <- liftIO $ Process.readProcessWithExitCode "nix-prefetch-git"
+      res <- liftIO $ Process.readProcessWithExitCode "nix-prefetch-git"
                ["--url", toS repo, "--rev", toS rev, "--hash", "sha1"] ""
-      case out of
+      case res of
         ((ExitFailure _), _, err) -> E.left $ toS err
         (ExitSuccess, out, _) -> E.hoistEither
           $ first (\decErr -> "parsing json output failed:\n"
