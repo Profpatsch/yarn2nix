1b5c1f92d835ba9d2d475b8cbfe02a105c55133a
Profpatsch <mail@profpatsch.de>
2018-02-07 14:35:29 +0100

    default.nix: patch hpack into that as well

--- default.nix
+++ default.nix
@@ -7,7 +7,14 @@
           (pkgs.callPackage ./nix-lib/old-version-dependencies.nix {})
           (self: super: {
             yarn2nix =
-              let pkg = self.callPackage ./yarn2nix.nix {};
+              let
+                pkg = oldpkgs.haskell.lib.overrideCabal
+                  (self.callPackage ./yarn2nix.nix {})
+                  (old: {
+                    prePatch = old.prePatch or "" + ''
+                      ${oldpkgs.lib.getBin self.hpack}/bin/hpack
+                    '';
+                  });
               in oldpkgs.haskell.lib.overrideCabal pkg (old: {
                 src = nix-lib.removePrefixes [ "nix-lib" "dist" "result" ] ./.;
               });
