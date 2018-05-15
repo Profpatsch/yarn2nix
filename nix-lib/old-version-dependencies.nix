fbddf374f87b6349e661ba2b845ee9842e1fce94
Profpatsch <mail@profpatsch.de>
2018-05-15 02:35:31 +0200

    bump yarn-lock to version 0.4.1

--- nix-lib/old-version-dependencies.nix
+++ nix-lib/old-version-dependencies.nix
@@ -3,34 +3,18 @@ self: super: {
   yarn-lock =
     super.mkDerivation rec {
       pname = "yarn-lock";
-      version = "0.4.0";
+      version = "0.4.1";
       # src = ../../../haskell/yarn-lock;
       src = pkgs.fetchFromGitHub {
         owner = "Profpatsch";
         repo = "yarn-lock";
-        # rev = version;
-        rev = "8a040e8250a8b871e0dafaeffccaf58ff34db0c3";
-        sha256 = "1r0k0p6ssay9nsh78f1pwkirp1y2qjwab73a7x2j7b9dp1djf2qx";
+        rev = version;
+        # rev = "61cc65a858db92e7c0bea861bf279f286c34bb81";
+        sha256 = "1fs440qf1c2w3dav82f1bsdi9k5cms106bg40ny33n0f0427mdpm";
       };
       license = lib.licenses.mit;
       buildDepends = with self; [ megaparsec protolude tasty-hunit tasty-th either neat-interpolation tasty-quickcheck quickcheck-instances ];
       buildTools = [ self.hpack ];
       prePatch = ''hpack'';
     };
-  megaparsec =
-     super.mkDerivation {
-       pname = "megaparsec";
-       version = "5.3.1";
-       sha256 = "06myn8l6jcbd494i3wr6q27npbbxd6c2gfkd2jdzwbjqjqbpv0j8";
-       libraryHaskellDepends = with self; [
-         base bytestring containers deepseq exceptions mtl
-         parser-combinators scientific text transformers
-       ];
-       testHaskellDepends = with self; [
-         base bytestring containers hspec hspec-expectations mtl QuickCheck
-         scientific text transformers
-       ];
-       benchmarkHaskellDepends = with self; [ base criterion deepseq text weigh ];
-       license = lib.licenses.bsd2;
-     };
 }
