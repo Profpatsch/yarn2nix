405e17f2b60dfa8816b216b53bae10c07dd25392
Profpatsch <mail@profpatsch.de>
2018-05-31 22:56:01 +0200

    default.nix: pin nixpkgs to unstable (by default)

--- default.nix
+++ default.nix
@@ -1,4 +1,9 @@
-{ nixpkgsPath ? <nixpkgs> }:
+{ nixpkgsPath ? builtins.fetchTarball {
+    # nixpkgs unstable 2018-05-31
+    url = "https://github.com/NixOS/nixpkgs/archive/5b468ea6b1d8d243847a05bdf5603e8abdfd7b4e.tar.gz";
+    sha256 = "0yj31a11h0mj9d5jyz95ynh5791nd0y1xzqk7ba3qrikljqg3lvb";
+  }
+}:
 (import nixpkgsPath {
   overlays = [(pkgs: oldpkgs: {
     haskellPackages =
@@ -22,7 +27,8 @@
                     '';
                   });
               in oldpkgs.haskell.lib.overrideCabal pkg (old: {
-                src = nix-lib.removePrefixes [ "nix-lib" "dist" "result" ] ./.;
+                src = nix-lib.removePrefixes
+                  [ "nix-lib" "dist" "result" ] ./.;
               });
           });
       };
