03dbefabfc61ffd3c5fe7b67e7ac6dfd97141de8
Profpatsch <mail@profpatsch.de>
2018-06-09 15:35:00 +0200

    default.nix: extract the pinned nixpkgs into separate file
    Will be used by yarn2nix-packages to use the same pinned version.

---
+++ nixpkgs-pinned.nix
@@ -0,0 +1,5 @@
+import (builtins.fetchTarball {
+  # nixpkgs unstable 2018-05-31
+  url = "https://github.com/NixOS/nixpkgs/archive/5b468ea6b1d8d243847a05bdf5603e8abdfd7b4e.tar.gz";
+  sha256 = "0yj31a11h0mj9d5jyz95ynh5791nd0y1xzqk7ba3qrikljqg3lvb";
+})
