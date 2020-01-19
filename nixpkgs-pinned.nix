fe440f8cac637ad1580d71b5aa5d9967f03dd306
Profpatsch <mail@profpatsch.de>
2020-01-19 23:39:32 +0100

    Bump nixpkgs pin to current unstable

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,5 +1,5 @@
 import (builtins.fetchTarball {
-  # nixpkgs unstable 2018-05-31
-  url = "https://github.com/NixOS/nixpkgs/archive/5b468ea6b1d8d243847a05bdf5603e8abdfd7b4e.tar.gz";
-  sha256 = "0yj31a11h0mj9d5jyz95ynh5791nd0y1xzqk7ba3qrikljqg3lvb";
+  # nixpkgs unstable 2019-05-31
+  url = "https://github.com/NixOS/nixpkgs/archive/c438ce12a858f24c1a2479213eaab751da45fa50.tar.gz";
+  sha256 = "18m4hxx8y0gfrmhkz29iyc0hmss584m9xhgpk7j7bwjaci0fps4z";
 })
