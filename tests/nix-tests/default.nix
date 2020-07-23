b77fa992df020b3ed468b540ad70cdc901970b3d
Profpatsch <mail@profpatsch.de>
2020-07-23 22:38:24 +0200

    fix(tests/nix-tests): create small tests for the --template output
    For now it just checks the verbatim output of a very simple
    package.json, but we should make sure to check all features we
    support.

--- tests/nix-tests/default.nix
+++ tests/nix-tests/default.nix
@@ -14,8 +14,54 @@ let
     assertEq
     ;
 
+  # small test package.json
+  my-package-json = pkgs.writeText "package.json" (builtins.toJSON {
+    name = "my-package";
+    version = "1.5.3";
+  });
+
+  # convert a package.json to yarn2nix package template
+  template = package-json: pkgs.runCommandLocal "generate-template" {} ''
+    ${yarn2nix}/bin/yarn2nix --template ${package-json} > $out
+    echo "template for ${package-json} is:" >&2
+    cat $out >&2
+  '';
+
+  # test suite
+  tests = runTestsuite "yarn2nix" [
+    (it "checks the template output"
+      (let tmpl = import (template my-package-json) {} {};
+      in [
+      # TODO: this is a naïve match, might want to create a better test
+      (assertEq "template" tmpl {
+        name = "my-package";
+        version = "1.5.3";
+        nodeBuildInputs = [];
+        meta = {
+          description = "";
+          homepage = "";
+          license = "";
+        };
+      })
+    ]))
+  ];
+
+  # small helper that checks the output of tests
+  # and pretty-prints errors if there were any
+  runTests = pkgs.runCommandLocal "run-tests" {
+    testOutput = builtins.toJSON tests;
+    passAsFile = [ "testOutput" ];
+  }
+    (if tests == {}
+     then ''touch $out''
+     else ''
+       echo "ERROR: some tests failed:" >&2
+       cat "$testOutputPath" | ${pkgs.jq}/bin/jq >&2
+       exit 1
+     '');
 
 in {
+  inherit runTests;
   testOverriding = import ./test-overriding.nix {
     inherit pkgs nixLib yarn2nix;
   };
