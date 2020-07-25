3a3d32d880765b55b19550687e6dca317dc1b7bd
sternenseemann <git@lukasepple.de>
2020-07-25 18:35:26 +0200

    test(nix-lib): test spdx license conversion

--- tests/nix-tests/default.nix
+++ tests/nix-tests/default.nix
@@ -14,11 +14,14 @@ let
     assertEq
     ;
 
-  # small test package.json
-  my-package-json = pkgs.writeText "package.json" (builtins.toJSON {
+  package-json-with-license = license: pkgs.writeText "package.json" (builtins.toJSON {
     name = "my-package";
     version = "1.5.3";
+    license = "${license}";
   });
+
+  # small test package.json
+  my-package-json = package-json-with-license "ISC";
 
   # convert a package.json to yarn2nix package template
   template = package-json: pkgs.runCommandLocal "generate-template" {} ''
@@ -30,10 +33,10 @@ let
   # test suite
   tests = runTestsuite "yarn2nix" [
     (it "checks the template output"
-      (let tmpl = import (template my-package-json) {} {};
+      (let tmpl = template my-package-json;
       in [
       # TODO: this is a naïve match, might want to create a better test
-      (assertEq "template" tmpl {
+      (assertEq "template" (import tmpl {} {}) {
         key = {
           name = "my-package";
           scope = "";
@@ -43,10 +46,35 @@ let
         meta = {
           description = "";
           homepage = "";
-          license = "";
+          license = "ISC";
+        };
+      })
+      (assertEq "called template" (nixLib.callTemplate tmpl {}) {
+        key = {
+          name = "my-package";
+          scope = "";
+        };
+        version = "1.5.3";
+        nodeBuildInputs = [];
+        meta = {
+          description = "";
+          homepage = "";
+          license = pkgs.lib.licenses.isc;
         };
       })
     ]))
+    (it "checks license conversion"
+      (builtins.map (x: assertEq x.i (nixLib.callTemplate
+        (template (package-json-with-license x.i)) {}).meta.license x.o)
+      [
+        { i = "UNLICENSED"; o = pkgs.lib.licenses.unfree; }
+        { i = "SEE LICENSE for info"; o = "SEE LICENSE for info"; }
+        # TODO this attr name is depreacated on nixpkgs master, replace with agpl3Only
+        { i = "AGPL-3.0-only"; o = pkgs.lib.licenses.agpl3; }
+        { i = "GPL-3.0-or-later"; o = pkgs.lib.licenses.gpl3Plus; }
+        { i = "BSD-3-Clause"; o = pkgs.lib.licenses.bsd3; }
+        { i = "MIT"; o = pkgs.lib.licenses.mit; }
+      ]))
   ];
 
   # small helper that checks the output of tests
