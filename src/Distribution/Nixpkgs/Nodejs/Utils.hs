93c183f510f6168db0240866e27c501a50c9b530
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-05 15:21:52 +0200

    feat(yarn2nix lib): lookup nixpkgs license sets by spdxId
    This feature is in preparation of generating proper meta sets in
    yarn2nix --template. We parse a licenses.json which is generated
    from the nixpkgs lib/licenses.nix using writeText "licenses.json"
    (builtins.toJSON lib.licenses) into a HashMap which uses the SPDX ID
    as a key. Then we are able to lookup an SPDX ID in this HashMap
    and return the nix expression representing the license referenced
    by this SPDX ID in nixpkgs.
    
    One limitation is that this approach is not dynamic, i. e. it
    doesn't benefit from updates in nixpkgs, as the set rendered to
    the template would be static instead of referencing the attribute
    inside of lib.licenses. Thus all license sets generated would be as
    up to date as the licenses.json used (probably as up to date as the
    nixpkgs version yarn2nix was built with). However, this is actually
    a feature: Any breaking changes to lib.licenses wouldn't break
    existing yarn2nix templates' evaluation.

--- src/Distribution/Nixpkgs/Nodejs/Utils.hs
+++ src/Distribution/Nixpkgs/Nodejs/Utils.hs
@@ -4,6 +4,7 @@ Description: Misc utils
 -}
 module Distribution.Nixpkgs.Nodejs.Utils where
 import Protolude
+import Nix.Expr
 import qualified Yarn.Lock.Types as YLT
 
 -- | Representation of a PackageKey as nix attribute name.
@@ -19,3 +20,13 @@ packageKeyNameToSymbol = \case
   YLT.ScopedPackageKey scope n -> "@" <> scope <> "/" <> n
 {-# INLINABLE packageKeyNameToSymbol #-}
 
+-- | Return a 'Binding' if 'Just' (or none if 'Nothing')
+--   for 'mkRecSet' and 'mkNonRecSet'.
+attrSetMay :: Text -> Maybe NExpr -> [Binding NExpr]
+attrSetMay k v = maybeToList $ (k $=) <$> v
+{-# INLINABLE attrSetMay #-}
+
+-- | Convenience shortcut for @'attrSetMay' x (mkStr \<$\> y)@.
+attrSetMayStr :: Text -> Maybe Text -> [Binding NExpr]
+attrSetMayStr k = attrSetMay k . fmap mkStr
+{-# INLINABLE attrSetMayStr #-}
