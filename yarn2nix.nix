8003b4789815b143c38839d6ec6f0ad3049fe4e3
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-05 17:07:24 +0200

    chore(yarn2nix.nix): update via cabal2nix
    Minus references to hpack since this seems to not work with the used
    version of nixpkgs.

--- yarn2nix.nix
+++ yarn2nix.nix
@@ -1,35 +1,34 @@
-{ mkDerivation, aeson, ansi-wl-pprint, async-pool, base, bytestring
-, containers, data-fix, directory, either, filepath, hnix
-, http-client, http-client-tls, monad-par, mtl, neat-interpolation
-, optparse-applicative, process, protolude, regex-tdfa
-, regex-tdfa-text, stdenv, stm, tasty, tasty-hunit
-, tasty-quickcheck, tasty-th, text, unix, unordered-containers
-, yarn-lock
+{ mkDerivation, aeson, async-pool, base, bytestring, containers
+, data-fix, directory, filepath, hnix, mtl
+, neat-interpolation, optparse-applicative, prettyprinter, process
+, protolude, regex-tdfa, regex-tdfa-text, stdenv, stm, tasty
+, tasty-hunit, tasty-quickcheck, tasty-th, text, transformers, unix
+, unordered-containers, yarn-lock
 }:
 mkDerivation {
   pname = "yarn2nix";
-  version = "0.6.0";
+  version = "0.8.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson ansi-wl-pprint async-pool base bytestring containers data-fix
-    directory either filepath hnix http-client http-client-tls
-    monad-par mtl process protolude regex-tdfa regex-tdfa-text stm text
+    aeson async-pool base bytestring containers data-fix directory
+    filepath hnix mtl optparse-applicative prettyprinter process
+    protolude regex-tdfa regex-tdfa-text stm text transformers
     unordered-containers yarn-lock
   ];
   executableHaskellDepends = [
-    aeson ansi-wl-pprint async-pool base bytestring containers data-fix
-    directory either filepath hnix http-client http-client-tls
-    monad-par mtl optparse-applicative process protolude regex-tdfa
-    regex-tdfa-text stm text unix unordered-containers yarn-lock
+    aeson async-pool base bytestring containers data-fix directory
+    filepath hnix mtl optparse-applicative prettyprinter process
+    protolude regex-tdfa regex-tdfa-text stm text transformers unix
+    unordered-containers yarn-lock
   ];
   testHaskellDepends = [
-    aeson ansi-wl-pprint async-pool base bytestring containers data-fix
-    directory either filepath hnix http-client http-client-tls
-    monad-par mtl neat-interpolation process protolude regex-tdfa
-    regex-tdfa-text stm tasty tasty-hunit tasty-quickcheck tasty-th
-    text unordered-containers yarn-lock
+    aeson async-pool base bytestring containers data-fix directory
+    filepath hnix mtl neat-interpolation optparse-applicative
+    prettyprinter process protolude regex-tdfa regex-tdfa-text stm
+    tasty tasty-hunit tasty-quickcheck tasty-th text transformers
+    unordered-containers yarn-lock
   ];
   homepage = "https://github.com/Profpatsch/yarn2nix#readme";
   description = "Convert yarn.lock files to nix expressions";
