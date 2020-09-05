4eaf77fd11faaec54a757b84d657bd9b630d7cfe
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-05 17:02:01 +0200

    feat(yarn2nix): arg parsing using optparse-applicative
    Unify runtime "configuration" mechanism: We now generate a RunConfig
    from the command line and pass it to cli. This config is used to
    determine which action to take (template / lock file) via RunMode
    and contains additional information to influence runtime behavior
    like runOffline.
    
    The configuration can be passed to both actions if necessary, mandatory
    action specific flags / variables could be hidden away in RunMode in the
    future.

--- Main.hs
+++ Main.hs
@@ -1,10 +1,34 @@
 module Main where
 
 import Distribution.Nixpkgs.Nodejs.Cli (cli)
-import qualified Data.Text as T
-import System.Environment (getArgs)
+import Distribution.Nixpkgs.Nodejs.RunConfig
+import Options.Applicative
 
 main :: IO ()
-main = getArgs >>= cli . map T.pack
+main = execParser (info (runConfigParser <**> helper) desc) >>= cli
 
+desc :: InfoMod a
+desc = fullDesc
+  <> progDesc (mconcat
+   [ "yarn2nix has two modes: "
+   , "In its default mode (started without --template) it parses a given "
+   , "yarn.lock file and prints a nix expressions representing it to stdout. "
+   , "If --template is given, it processes a given package.json and prints a "
+   , "template nix expression for an equivalent nix package. "
+   , "In both modes yarn2nix will take the file as an argument or read it from "
+   , "stdin if it is missing." ])
 
+-- If --template is given, run in NodeTemplate mode,
+-- otherwise the default mode YarnLock is used.
+runModeParser :: Parser RunMode
+runModeParser = flag YarnLock NodeTemplate $
+     long "template"
+  <> help "Output a nix package template for a given package.json"
+
+runConfigParser :: Parser RunConfig
+runConfigParser = RunConfig
+  <$> runModeParser
+  <*> switch
+      (long "offline"
+    <> help "Makes yarn2nix fail if network access is required")
+  <*> optional (argument str (metavar "FILE"))
---
+++ src/Distribution/Nixpkgs/Nodejs/RunConfig.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-|
+Description: Invocation Configuration
+-}
+module Distribution.Nixpkgs.Nodejs.RunConfig
+  ( RunConfig (..)
+  , RunMode (..)
+  ) where
+
+import Protolude
+
+-- | Type of action @yarn2nix@ is performing.
+data RunMode
+  = YarnLock     -- ^ Output a nix expression for a @yarn.lock@
+  | NodeTemplate -- ^ Output a nix template corresponding to a @package.json@
+  deriving (Show, Eq)
+
+-- | Runtime configuration of @yarn2nix@. Typically this is determined from
+--   its command line arguments and valid for the current invocation only.
+data RunConfig
+  = RunConfig
+  { runMode         :: RunMode
+  , runOffline      :: Bool            -- ^ If @True@, @yarn2nix@ will fail if it
+                                       --   requires network access. Currently this means
+                                       --   'Distribution.Nixpkgs.Nodejs.ResolveLockfile.resolveLockfileStatus'
+                                       --   will throw an error in case resolving a hash
+                                       --   requires network access.
+  , runInputFile    :: Maybe FilePath  -- ^ File to process. If missing the appropriate
+                                       --   file for the current mode from the current
+                                       --   working directory is used.
+  } deriving (Show, Eq)
