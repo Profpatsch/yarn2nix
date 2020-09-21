8365b0b6a5600ff0c2d932ed872a3096151d13cf
Profpatsch <mail@profpatsch.de>
2020-09-21 19:09:13 +0200

    fix(Main): improve the usage text linebreaks

--- Main.hs
+++ Main.hs
@@ -1,22 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Protolude
 import Distribution.Nixpkgs.Nodejs.Cli (cli)
 import Distribution.Nixpkgs.Nodejs.RunConfig
 import Options.Applicative
+import Options.Applicative.Help.Pretty (linebreak)
 
 main :: IO ()
 main = execParser (info (runConfigParser <**> helper) desc) >>= cli
 
 desc :: InfoMod a
 desc = fullDesc
-  <> progDesc (mconcat
-   [ "yarn2nix has two modes: "
-   , "In its default mode (started without --template) it parses a given "
-   , "yarn.lock file and prints a nix expressions representing it to stdout. "
-   , "If --template is given, it processes a given package.json and prints a "
-   , "template nix expression for an equivalent nix package. "
-   , "In both modes yarn2nix will take the file as an argument or read it from "
-   , "stdin if it is missing." ])
+  <> progDescDoc (Just $ mconcat $ intersperse linebreak
+   [ "yarn2nix has two modes:"
+   <> linebreak
+   , "In its default mode (started without --template) it parses a given yarn.lock file"
+   , "and prints a nix expressions representing it to stdout."
+   <> linebreak
+   , "If --template is given, it processes a given package.json"
+   , "and prints a template nix expression for an equivalent nix package."
+   <> linebreak
+   , "In both modes yarn2nix will take the file as an argument"
+   , "or read it from stdin if it is missing."
+   ])
 
 -- If --template is given, run in NodeTemplate mode,
 -- otherwise the default mode YarnLock is used.
