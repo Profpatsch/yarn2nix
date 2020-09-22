65460bd54b096ae009e55fc7858ecc0b14cde5c2
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-22 14:08:58 +0200

    feat(yarn2nix): return of dieWithUsage
    Use parserFailure and renderFailure to obtain the help text.
    optparse-applicative changes the constructor of ShowHelpText so we
    introduce an upper constraint.

--- Main.hs
+++ Main.hs
@@ -1,41 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Protolude
 import Distribution.Nixpkgs.Nodejs.Cli (cli)
-import Distribution.Nixpkgs.Nodejs.RunConfig
-import Options.Applicative
-import Options.Applicative.Help.Pretty (linebreak)
 
 main :: IO ()
-main = execParser (info (runConfigParser <**> helper) desc) >>= cli
-
-desc :: InfoMod a
-desc = fullDesc
-  <> progDescDoc (Just $ mconcat $ intersperse linebreak
-   [ "yarn2nix has two modes:"
-   <> linebreak
-   , "In its default mode (started without --template) it parses a given yarn.lock file"
-   , "and prints a nix expressions representing it to stdout."
-   <> linebreak
-   , "If --template is given, it processes a given package.json"
-   , "and prints a template nix expression for an equivalent nix package."
-   <> linebreak
-   , "In both modes yarn2nix will take the file as an argument"
-   , "or read it from stdin if it is missing."
-   ])
-
--- If --template is given, run in NodeTemplate mode,
--- otherwise the default mode YarnLock is used.
-runModeParser :: Parser RunMode
-runModeParser = flag YarnLock NodeTemplate $
-     long "template"
-  <> help "Output a nix package template for a given package.json"
-
-runConfigParser :: Parser RunConfig
-runConfigParser = RunConfig
-  <$> runModeParser
-  <*> switch
-      (long "offline"
-    <> help "Makes yarn2nix fail if network access is required")
-  <*> optional (argument str (metavar "FILE"))
+main = cli
--- src/Distribution/Nixpkgs/Nodejs/Cli.hs
+++ src/Distribution/Nixpkgs/Nodejs/Cli.hs
@@ -11,10 +11,13 @@ import Protolude
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import qualified Options.Applicative as O
+import qualified Options.Applicative.Help.Pretty as O (linebreak)
 import qualified System.Directory as Dir
+import System.Environment (getProgName)
 
 import qualified Nix.Pretty as NixP
-import Data.Text.Prettyprint.Doc.Render.Text (putDoc)
+import qualified Data.Text.Prettyprint.Doc.Render.Text as RT
 import qualified Yarn.Lock as YL
 import qualified Yarn.Lock.Types as YLT
 import qualified Yarn.Lock.Helpers as YLH
@@ -25,15 +28,36 @@ import qualified Distribution.Nixpkgs.Nodejs.ResolveLockfile as Res
 import qualified Distribution.Nodejs.Package as NP
 import Distribution.Nixpkgs.Nodejs.RunConfig
 
+description :: O.InfoMod a
+description = O.fullDesc
+  <> O.progDescDoc (Just $ mconcat $ intersperse O.linebreak
+   [ "yarn2nix has two modes:"
+   <> O.linebreak
+   , "In its default mode (started without --template) it parses a given yarn.lock file"
+   , "and prints a nix expressions representing it to stdout."
+   <> O.linebreak
+   , "If --template is given, it processes a given package.json"
+   , "and prints a template nix expression for an equivalent nix package."
+   <> O.linebreak
+   , "In both modes yarn2nix will take the file as an argument"
+   , "or read it from stdin if it is missing."
+   ])
+
+-- | Main entry point for @yarn2nix@.
+cli :: IO ()
+cli = parseOpts >>= runAction
+
 fileFor :: RunConfig -> Text
 fileFor cfg =
   case runMode cfg of
     YarnLock -> "yarn.lock"
     NodeTemplate -> "package.json"
 
--- | Main entry point for @yarn2nix@.
-cli :: RunConfig -> IO ()
-cli cfg = do
+parseOpts :: IO RunConfig
+parseOpts = O.customExecParser optparsePrefs runConfigParserWithHelp
+
+runAction :: RunConfig -> IO ()
+runAction cfg = do
   file <- fileForConfig
   case runMode cfg of
     YarnLock -> parseYarn file
@@ -45,7 +69,7 @@ cli cfg = do
         Just f -> pure f
         Nothing -> Dir.getCurrentDirectory >>= \d ->
           Dir.findFile [d] (toS $ fileFor cfg) >>= \case
-            Nothing -> die'
+            Nothing -> dieWithUsage
               $ "No " <> fileFor cfg <> " found in current directory"
             Just path -> pure path
     parseYarn :: FilePath -> IO ()
@@ -53,7 +77,7 @@ cli cfg = do
       let pathT = toS path
       fc <- readFile path
         `catch` \e
-          -> die' ("Unable to open " <> pathT <> ":\n" <> show (e :: IOException))
+          -> dieWithUsage ("Unable to open " <> pathT <> ":\n" <> show (e :: IOException))
       case YL.parse path fc of
         Right yarnfile  -> toStdout cfg yarnfile
         Left err -> die' ("Could not parse " <> pathT <> ":\n" <> show err)
@@ -65,8 +89,44 @@ cli cfg = do
           print $ NixP.prettyNix $ NodeFP.genTemplate nodeModule
         Left err -> die' ("Could not parse " <> toS path <> ":\n" <> show err)
 
+-- get rid of odd linebreaks by increasing width enough
+optparsePrefs :: O.ParserPrefs
+optparsePrefs = O.defaultPrefs { O.prefColumns = 100 }
+
+-- If --template is given, run in NodeTemplate mode,
+-- otherwise the default mode YarnLock is used.
+runModeParser :: O.Parser RunMode
+runModeParser = O.flag YarnLock NodeTemplate $
+     O.long "template"
+  <> O.help "Output a nix package template for a given package.json"
+
+runConfigParser :: O.Parser RunConfig
+runConfigParser = RunConfig
+  <$> runModeParser
+  <*> O.switch
+      (O.long "offline"
+    <> O.help "Makes yarn2nix fail if network access is required")
+  <*> O.optional (O.argument O.str (O.metavar "FILE"))
+
+runConfigParserWithHelp :: O.ParserInfo RunConfig
+runConfigParserWithHelp =
+  O.info (runConfigParser <**> O.helper) description
+ 
 die' :: Text -> IO a
 die' err = putErrText err *> exitFailure
+
+-- TODO from optparse-applicative 0.16.0.0 ShowHelpText
+-- accepts an error message as argument, so we can use
+-- that instead of putErrText.
+dieWithUsage :: Text -> IO a
+dieWithUsage err = do
+  putErrText (err <> "\n")
+  progn <- getProgName
+  hPutStr stderr
+    . fst . flip O.renderFailure progn
+    $ O.parserFailure optparsePrefs
+        runConfigParserWithHelp O.ShowHelpText mempty
+  exitFailure
 
 -- TODO refactor
 toStdout :: RunConfig -> YLT.Lockfile -> IO ()
@@ -80,4 +140,4 @@ toStdout cfg lf = do
     Left err -> die' (T.intercalate "\n" $ toList err)
     Right res -> pure res
   -- killThread thrd
-  putDoc $ NixP.prettyNix $ NixOut.mkPackageSet $ NixOut.convertLockfile lf'
+  RT.putDoc $ NixP.prettyNix $ NixOut.mkPackageSet $ NixOut.convertLockfile lf'
