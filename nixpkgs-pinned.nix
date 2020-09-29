6a0520e0d0ce18afd5a5f867430b9418717005bd
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-29 10:03:40 +0200

    chore(default.nix): bump nixpkgs
    regex-tdfa-text was the offender preventing us from updating, however,
    regex-tdfa-text has been merged into regex-tdfa, so we only need to bump
    some version constraints.

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,5 +1,5 @@
 import (builtins.fetchTarball {
-  # nixpkgs unstable 2019-05-31
-  url = "https://github.com/NixOS/nixpkgs/archive/c438ce12a858f24c1a2479213eaab751da45fa50.tar.gz";
-  sha256 = "18m4hxx8y0gfrmhkz29iyc0hmss584m9xhgpk7j7bwjaci0fps4z";
+  # nixpkgs unstable 2020-09-29
+  url = "https://github.com/NixOS/nixpkgs/archive/8ed3178bc2050b93ed14d1b0862706f6c8845d08.tar.gz";
+  sha256 = "1mf0ssjpzw4gmi8vfqr4h0c4qfpjv2gzpx5svawb4qpdi1f6fmxs";
 })
