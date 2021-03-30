b2fdc41ee4d468579a91b40e2b26c834e06f0eb9
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-03-30 11:04:10 +0200

    chore(default.nix): fix indent
    3 spaces are kinda fancy though

--- default.nix
+++ default.nix
@@ -7,47 +7,47 @@ let
     inherit pkgs;
   };
 
-   static = pkgs.haskell.lib.justStaticExecutables haskellPackages.yarn2nix;
+  static = pkgs.haskell.lib.justStaticExecutables haskellPackages.yarn2nix;
 
-   yarn2nix = pkgs.stdenv.mkDerivation {
-     name = "yarn2nix";
-     src = pkgs.nix-gitignore.gitignoreSource [ ".git/" ] ./yarn2nix;
-     outputs = [ "bin" "doc" "out" ];
-     phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
-     installPhase = ''
-       install -D --target-directory=$bin/bin ${static}/bin/*
-       ${pkgs.skawarePackages.cleanPackaging.commonFileActions {
-          noiseFiles = [
-            "tests/*"
-            "src/*"
-            "Main.hs"
-            "Setup.hs"
-            "package.yaml"
-            "yarn2nix.cabal"
-            ".envrc"
-            "shell.nix"
-            "Repl.hs"
-            "nixpkgs-pinned.nix"
-            "yarn2nix.nix"
-            "default.nix"
-            "NodePackageTool.hs"
-            ".gitignore"
-            ".github"
-            "nix-lib"
-          ];
-          docFiles = [
-            "README.md"
-            "LICENSE"
-            "CHANGELOG.md"
-          ];
-        }} "$doc/share/yarn2nix"
-       ${pkgs.skawarePackages.cleanPackaging.checkForRemainingFiles}
-     '';
+  yarn2nix = pkgs.stdenv.mkDerivation {
+    name = "yarn2nix";
+    src = pkgs.nix-gitignore.gitignoreSource [ ".git/" ] ./yarn2nix;
+    outputs = [ "bin" "doc" "out" ];
+    phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
+    installPhase = ''
+      install -D --target-directory=$bin/bin ${static}/bin/*
+      ${pkgs.skawarePackages.cleanPackaging.commonFileActions {
+         noiseFiles = [
+           "tests/*"
+           "src/*"
+           "Main.hs"
+           "Setup.hs"
+           "package.yaml"
+           "yarn2nix.cabal"
+           ".envrc"
+           "shell.nix"
+           "Repl.hs"
+           "nixpkgs-pinned.nix"
+           "yarn2nix.nix"
+           "default.nix"
+           "NodePackageTool.hs"
+           ".gitignore"
+           ".github"
+           "nix-lib"
+         ];
+         docFiles = [
+           "README.md"
+           "LICENSE"
+           "CHANGELOG.md"
+         ];
+       }} "$doc/share/yarn2nix"
+      ${pkgs.skawarePackages.cleanPackaging.checkForRemainingFiles}
+    '';
 
-     passthru.nixLib = import ./yarn2nix/nix-lib {
-       inherit lib pkgs;
-       inherit yarn2nix;
-     };
-   };
+    passthru.nixLib = import ./yarn2nix/nix-lib {
+      inherit lib pkgs;
+      inherit yarn2nix;
+    };
+  };
 
 in yarn2nix
