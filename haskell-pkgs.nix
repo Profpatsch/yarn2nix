d87a823120ae2ad9b54ee4891858c263c7eae932
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-03-30 11:40:18 +0200

    refactor(haskell-pkgs): clean up haskell overlay
    * reduce code duplication by introducing minimalHaskellSource
    * remove unnecessary call to hpack in yarn2nix (haskellPackages'
      mkDerivation does that already for us in prePatch)
    * remove the double overrideCabal for yarn2nix and merge them into a
      single one

--- haskell-pkgs.nix
+++ haskell-pkgs.nix
@@ -5,6 +5,20 @@ let
 
   licensesJson = pkgs.writeText "licenses.json"
     (builtins.toJSON (lib.filterAttrs (n: v: v ? spdxId) lib.licenses));
+
+  minimalHaskellSource = root: extra:
+    builtins.path {
+      path = root;
+      name = "${builtins.baseNameOf root}-source";
+      filter = path: type:
+        lib.any (p: lib.hasPrefix (toString root + "/" + p) path) ([
+          "package.yaml"
+          "LICENSE"
+          "CHANGELOG.md"
+          "src"
+          "tests"
+        ] ++ extra);
+    };
 in
 
 pkgs.haskellPackages.override {
@@ -14,54 +28,29 @@ pkgs.haskellPackages.override {
         let
           pkg = self.callPackage ./yarn-lock/yarn-lock.nix {};
         in pkgs.haskell.lib.overrideCabal pkg (old: {
-          src = builtins.filterSource
-            (path: type:
-               if lib.any (p: lib.hasPrefix (toString ./yarn-lock + "/" + p) path) [
-                 "package.yaml"
-                 "LICENSE"
-                 "CHANGELOG.md"
-                 "src"
-                 "tests"
-               ]
-               then true
-               else false
-            ) ./yarn-lock;
+          version = "git";
+          src = minimalHaskellSource ./yarn-lock [];
         });
-
 
       yarn2nix =
         let
-          pkg = pkgs.haskell.lib.overrideCabal
-            (self.callPackage ./yarn2nix/yarn2nix.nix {})
-            (old: {
-              prePatch = ''
-                ${pkgs.hpack}/bin/hpack
-                # we depend on the git prefetcher
-                substituteInPlace \
-                  src/Distribution/Nixpkgs/Nodejs/ResolveLockfile.hs \
-                  --replace '"nix-prefetch-git"' \
-                    '"${pkgs.nix-prefetch-git.override { git = pkgs.gitMinimal; }}/bin/nix-prefetch-git"'
-                sed -i '/license-data/a \ <> O.value "${licensesJson}"' \
-                  src/Distribution/Nixpkgs/Nodejs/Cli.hs
-              '';
-            });
+          pkg = self.callPackage ./yarn2nix/yarn2nix.nix {};
         in pkgs.haskell.lib.overrideCabal pkg (old: {
-          src = builtins.filterSource
-            (path: type:
-               if lib.any (p: lib.hasPrefix (toString ./yarn2nix + "/" + p) path) [
-                 "package.yaml"
-                 "README.md"
-                 "nix-lib"
-                 "LICENSE"
-                 "src"
-                 "NodePackageTool.hs"
-                 "Main.hs"
-                 "tests"
-               ]
-               then true
-               else false
-            ) ./yarn2nix;
-        });
-    });
- }
+          version = "git";
+          src = minimalHaskellSource ./yarn2nix [
+            "Main.hs"
+            "NodePackageTool.hs"
+          ];
 
+          prePatch = ''
+            # we depend on the git prefetcher
+            substituteInPlace \
+              src/Distribution/Nixpkgs/Nodejs/ResolveLockfile.hs \
+              --replace '"nix-prefetch-git"' \
+                '"${pkgs.nix-prefetch-git.override { git = pkgs.gitMinimal; }}/bin/nix-prefetch-git"'
+            sed -i '/license-data/a \ <> O.value "${licensesJson}"' \
+              src/Distribution/Nixpkgs/Nodejs/Cli.hs
+          '' + old.prePatch or "";
+        });
+      });
+   }
