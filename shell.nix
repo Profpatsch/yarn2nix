e000c4ba464e9dd20df8ca3080a1f4f81f12707c
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2021-03-30 11:40:34 +0200

    feat(shell.nix): use haskellPackages.shellFor
    Instead of manually listing the package dependencies we can use
    haskellPackages.shellFor which creates an environment for a list of
    packages and also allows injecting extra build tools plus a hoogle
    instance set up with all dependencies of the listed haskellPackages.

--- shell.nix
+++ shell.nix
@@ -1,43 +1,19 @@
+{ pkgs ? import ./nixpkgs-pinned.nix {} }:
+
 let
-  pkgs = import ./nixpkgs-pinned.nix {};
+  haskellPackages = import ./haskell-pkgs.nix {
+    inherit pkgs;
+  };
 in
-(pkgs.haskellPackages.override {
-  overrides =
-    (self: super: {
-      my-pkg = let
-        buildDepends = with self; [
-          aeson
-          ansi-wl-pprint
-          async-pool
-          either
-          hnix
-          http-client-tls
-          monad-par
-          neat-interpolation
-          old-locale
-          old-time
-          protolude
-          quickcheck-instances
-          regex-tdfa
-          tasty-hunit
-          tasty-quickcheck
-          tasty-th
-        ];
-        in super.mkDerivation {
-          pname = "pkg-env";
-          src = "/dev/null";
-          version = "none";
-          license = "none";
-          inherit buildDepends;
-          buildTools = [
-            pkgs.hpack
-            pkgs.cabal2nix
-            self.ghcid
-            pkgs.cabal-install
-            (self.hoogleLocal {
-              packages = buildDepends;
-            })
-          ];
-        };
-      });
-}).my-pkg.env
+  haskellPackages.shellFor {
+    packages = hps: [
+      hps.yarn2nix
+      hps.yarn-lock
+    ];
+    withHoogle = true;
+    buildInputs = [
+      pkgs.cabal-install
+      pkgs.hpack
+      pkgs.ghcid
+    ];
+  }
